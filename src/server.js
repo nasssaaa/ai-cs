3ab6d95a4d57833c60262a24d08036d5402484d0
wwh <1776569851@qq.com>
2026-01-17 14:22:10 +0800

    'update'

--- src/server.js
+++ src/server.js
@@ -91,7 +91,7 @@ async function updateTokensUsage(date, tokens) {
 
 //ai应用调用函数
 async function getAiResponse(prompt, history) {
-    const appId = 'kb-service-b4a688bcf5ab75bf'
+    const appId = 'kb-service-a2f65c12c4ab9f7a'
     const apiKey = 'afe01879-d881-45f6-bbb4-fc8a34390aa5'
 
     const url = `https://api-knowledgebase.mlp.cn-beijing.volces.com/api/knowledge/service/chat`;
